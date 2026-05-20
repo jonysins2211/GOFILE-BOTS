4a2c695cd900778c2bd21eb46c1c7831fbdbc635
jonysins2211 <Jonysins11220@gmail.com>
2026-05-21 02:50:16 +0530

    Enable concurrent Telegram file transmissions

--- bot.py
+++ bot.py
@@ -53,7 +53,8 @@ app = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    workers=10
+    workers=10,
+    max_concurrent_transmissions=10
 )
 
 download_queue = Queue()
